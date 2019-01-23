c934cf80a9a61e23b1f566a6c25fd096d9e6098c
2ps <pshingavi@gmail.com>
2019-01-22 21:05:49 -0600

    Initial commit with shell script and buildspec

---
+++ build_git.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+version=${1:-"2.20.1"}
+apt-get -qq update
+apt-get -qq install software-properties-common
+apt-get -qq install dh-autoreconf libcurl4-openssl-dev libexpat1-dev gettext \
+  libz-dev libssl-dev python python-pip
+yum -y -q install asciidoc xmlto docbook2X autoconf
+pip install -q awscli
+mkdir -p /u/downloads
+version=${1:-"2.20.1"}
+cd /u/downloads
+filename="/u/downloads/git-${version}.tgz"
+printf "downloading source bundle\n"
+wget -q "https://www.kernel.org/pub/software/scm/git/git-${version}.tar.gz" -O "${filename}" ;
+build_dir=/u/git-${version}
+mkdir -p ${build_dir} && cd ${build_dir}
+tar xzf $filename --strip-components=1
+printf "\e[32mbuilding!\e[0m\n"
+make configure
+./configure --prefix=/usr
+make all doc
